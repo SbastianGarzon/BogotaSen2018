fae20ee613b32d9d5eea18f7b455f7e8652221fe
Juan Sebastian Garzón Alvarado <js.garzon398@uniandes.edu.co>
2018-03-17 02:27:13 -0500

    Update MigrarCompletos.sh

--- localidades/SinDatos/MigrarCompletos.sh
+++ localidades/SinDatos/MigrarCompletos.sh
@@ -1,4 +1,4 @@
-#Este script migra los datos de votacion que ya estan con datos a una nueva carpeta
+#Este script migra los archivos de votacion que ya estan con datos a una nueva carpeta
 for filename in *_OK.csv
 do
 	mv $filename ../Votaciones_localidades/$filename
